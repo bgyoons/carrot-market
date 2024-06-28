1a8def507d2961937246c09dc0cfef6bdb3fc6c7
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-06-28 11:57:17 +0900

    Style: 초기 스타일링

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="flex flex-col items-center w-screen min-h-screen bg-slate-800 text-white">{children}</body>
     </html>
   );
 }
