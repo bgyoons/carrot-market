f1175d224099f9578a4545a3af362ca8bde05204
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-07-03 14:02:53 +0900

    Remove: 필요없는 코드 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,11 @@
-import Link from "next/link";
-import Worker from "./(component)/Worker";
-
-interface Worker {
-  id: string;
-  name: string;
-  squareImage: string;
-  industries: string[];
-  netWorth: number;
-}
-
-const URL = "https://billions-api.nomadcoders.workers.dev/";
-
 export const metadata = {
   title: "Home"
 };
 
-async function getWorkerList() {
-  const response = await fetch(URL);
-  const json = await response.json();
-  return json;
-}
 
-export default async function Home() {
-  const workerList: Worker[] = await getWorkerList();
-
+export default function Home() {
   return (
-    <main className="w-[1280px] min-h-full flex flex-wrap">
-      {workerList.slice(0, 50).map(worker => {
-        const name = worker.name.toLowerCase().replace(" ", "-");
-        return (<Link key={worker.id} href={`person/${name}`}>
-          <Worker worker={worker} />
-        </Link>);
-      }
-      )}
+    <main className="w-[1280px] min-h-full flex">
     </main>
   );
 }
