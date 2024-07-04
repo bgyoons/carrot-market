bff4be3d48052e11ca5a638cb4f38df9973e4ef8
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-07-04 13:10:20 +0900

    Feat: server action & FormData 받아오기 위한 name 속성 추가

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,18 @@ export const metadata = {
 
 
 export default function Home() {
+
+  const handleForm = async (formData: FormData) => {
+    "use server";
+  };
+
   return (
     <main className="h-full flex flex-col w-80 m-auto justify-center *:font-medium">
-      <form className="flex flex-col w-full" action="">
-        <Input type="email" placeholder="Email" required errors={[]} />
-        <Input type="text" placeholder="Username" required errors={[]} />
-        <Input type="password" placeholder="Password" required errors={[]} />
         <Button text="Log in" loading={true} />
+      <form className="flex flex-col w-full" action={handleForm}>
+        <Input name="email" type="email" placeholder="Email" required errors={[]} />
+        <Input name="username" type="text" placeholder="Username" required errors={[]} />
+        <Input name="password" type="password" placeholder="Password" required errors={[]} />
       </form>
     </main>
   );
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,6 +1,7 @@
 interface InputProps {
   placeholder: string,
   type: string,
+  name: string,
   required: boolean,
   errors: string[];
 }
@@ -8,12 +9,14 @@ interface InputProps {
 export default function Input({
   type,
   placeholder,
+  name,
   required,
   errors
 }: InputProps) {
   return (
     <div>
       <input
+        name={name}
         className="w-full outline-none focus:ring-2 ring-1 focus:ring-indigo-300 ring-slate-300 py-2 px-4 mb-3 rounded-full"
         type={type}
         placeholder={placeholder}
