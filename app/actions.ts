b9c5886e0251fa4ee24ac370773c7333b08bb2d3
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-07-06 01:54:23 +0900

    Remove: 예전 과제 코드 삭제

--- app/actions.ts
+++ app/actions.ts
@@ -1,20 +1,5 @@
 "use server";
 
-import { PASSWORD } from "./constants";
 
 export const handleForm = async (prevState: any, formData: FormData) => {
-  const response = await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const password = formData.get("password");
-      if (password === PASSWORD) resolve(null);
-      else reject();
-    }, 2000);
-  }).then(() => ({
-    status: "success",
-    error: ""
-  })).catch(() => ({
-    status: "error",
-    error: "Wrong Password"
-  }));
-  return response;
 };
