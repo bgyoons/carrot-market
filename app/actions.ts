a7b18dec9f88dd845ada6e5502135b7b37222c61
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-07-06 02:30:23 +0900

    Feat: 유효성 검사 결과 return

--- app/actions.ts
+++ app/actions.ts
@@ -11,4 +11,18 @@ const formSchema = z.object({
 });
 
 export const handleForm = async (prevState: any, formData: FormData) => {
+  const data = {
+    username: formData.get("username"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+
+  const result = formSchema.safeParse(data);
+
+  if (!result.success) {
+    return {
+      ...result,
+      error: result.error.flatten()
+    };
+  } else return result;
 };
