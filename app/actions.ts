a5e6cc4c5372b7faf29dc286c1d53ecda79a434f
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-07-06 02:06:18 +0900

    Feat: zod 이용해서 form 스키마 생성 및 유효성 검사

--- app/actions.ts
+++ app/actions.ts
@@ -1,5 +1,16 @@
 "use server";
 
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(5),
+  email: z.string().email().includes("@zod.com"),
+  password: z.string().min(10).refine(password => {
+    for (const ele of password) {
+      if (!isNaN(+ele)) return true;
+    } return false;
+  }, "password should contain at least one number"),
+});
 
 export const handleForm = async (prevState: any, formData: FormData) => {
 };
