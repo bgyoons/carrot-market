b84afd6b4ab8a67ca9c204b973c2ea199378e1f6
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-07-06 02:17:20 +0900

    Fix: 비밀번호 유효성 검사 정규표현식으로 변경

--- app/actions.ts
+++ app/actions.ts
@@ -2,16 +2,12 @@
 
 import { z } from "zod";
 
-const checkPassword = (password: string) => {
-  for (const ele of password) {
-    if (!isNaN(+ele)) return true;
-  } return false;
-};
+const passwordRegex = new RegExp(/[0-9]/g);
 
 const formSchema = z.object({
   username: z.string().min(5),
   email: z.string().email().includes("@zod.com"),
-  password: z.string().min(10).refine(checkPassword, "password should contain at least one number"),
+  password: z.string().min(10).regex(passwordRegex, "password should contain at least one number"),
 });
 
 export const handleForm = async (prevState: any, formData: FormData) => {
